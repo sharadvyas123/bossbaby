3a9a496a6cd9aee2bc908499a57287254ed126bd
sharadvyas123 <sharadvyas132@gmail.com>
2026-01-16 22:39:52 +0530

    try

--- src/lib/googleCalendar.js
+++ src/lib/googleCalendar.js
@@ -12,12 +12,15 @@ function create30MinSlot(date, timeSlot) {
 }
 
 // 🔹 GOOGLE AUTH (VERCEL SAFE)
-const credentials = JSON.parse(
-  process.env.GOOGLE_SERVICE_ACCOUNT_KEY.replace(/\\n/g, "\n")
-);
+
 
 const auth = new google.auth.GoogleAuth({
-  credentials,
+  credentials:{
+    type:"service_account",
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  },
   scopes: ["https://www.googleapis.com/auth/calendar"],
 });
 
